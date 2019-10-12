7e6e2ed0b93b67f5b3cc54e3078841ce4668d1eb
LuukOudshoorn25 <luuk@lupanas.nl>
2019-10-12 18:47:11 +0200

    Updated the FLT photometry script

--- calibrate_wfc3_flt.py
+++ calibrate_wfc3_flt.py
@@ -226,6 +226,67 @@ def sort_files():
     os.chdir('../')
 
 
+def GetCRMasked_exptime(flist, this_file_path, folderpath, exptime):
+#flist, this_file_path, folderpath, exptime = this_wfc_ims, im, folder, hdu.header['EXPTIME']
+    storepath_median = folderpath + ('wfc1' if 'wfc1' in this_file_path else 'wfc2') + 'median.fits'  
+    this_file = fits.open(this_file_path)[1].data / exptime
+    offset_file = this_file_path.replace('_pamcorr_exptime.fits', '_offset.fits')
+    try:
+        offsets = fits.open(offset_file)[0].data
+        print('Reading from offset fits')
+    except:
+        if len(flist)>6:
+            flist = np.random.choice(flist, 6)
+        all_frames = np.zeros((*this_file.shape, len(flist)))
+
+        
+        num_cores = max(6, len(flist))
+        inputs = zip(flist, len(flist)*[this_file_path])
+        results = Parallel(n_jobs=num_cores)(delayed(regrid_worker)(i) for i in [w for w in inputs])
+        for i,file in enumerate(flist):
+            all_frames[:,:,i] = results[i] / fits.open(file)[0].header['EXPTIME']
+        too_little = np.where(np.sum(all_frames==0, axis=2)>4, np.nan, 1)
+        medians = np.nanmedian(all_frames, axis=2)  * too_little
+        del all_frames
+        rmsmapfile = this_file_path.replace('exptime', 'rmsmap')
+        bg_stddev_arr = fits.open(rmsmapfile)[0].data
+        #if not os.path.exists(storepath_median):
+        fits.writeto(storepath_median, medians, overwrite=True)
+
+        offsets = np.abs(this_file - medians)
+    if not os.path.exists(offset_file):
+        offsets_hdu = fits.open(this_file_path)[1].copy()
+        offsets_hdu.data = offsets
+        offsets_hdu.writeto(offset_file, overwrite=True)
+    rmsmapfile = this_file_path.replace('exptime', 'rmsmap')
+    bg_stddev_arr = fits.open(rmsmapfile)[0].data
+    CRmask = np.where(offsets>10*bg_stddev_arr, 1, 0)
+    this_file_corr = this_file.copy()
+    this_file_corr[CRmask] = -1
+    return this_file_corr*exptime, np.median(bg_stddev_arr), CRmask
+
+
+def regrid_worker(input_tuple):
+    im, refim = input_tuple
+    ref_hdu  = fits.open(refim)[1]
+    ref_fobj = fits.open(refim.split('_wfc')[0]+'.fits')
+    ref_wcs  = WCS(ref_hdu, fobj=ref_fobj)
+
+    singleexphdu = fits.open(im)[1]
+    fobj = fits.open(im.split('_wfc')[0]+'.fits')
+    wcs = WCS(singleexphdu,fobj=fobj)
+    
+    root = im.split('/')[-1]
+    target_dir = '/'.join(im.split('/')[:-1])+'/'
+    target_dir = target_dir +root[:-5]+'_regrid.fits'
+
+    print("Single exposure read")
+    array, interp = reproject_interp((singleexphdu.data, wcs), ref_wcs, shape_out=singleexphdu.data.shape)
+    print("Reprojection done")
+    return array
+
+
+
 
 
 #create_dir_tree()
@@ -303,6 +364,40 @@ DO_APPHOT = True
 
 
 
+
+
+
+
+
+
+
+DO_RMSMAPS=True
+if DO_RMSMAPS:
+    os.system('cp ./SEXfiles/* ./working_dir/')
+    os.chdir('./working_dir')
+    ims = glob('../FLT_exposures/*/*/*_pamcorr_exptime.fits')
+    for im in ims:
+        root = im.split('/')[-1][:-5]
+        hdul = fits.open(im)
+        hdul[1].data = hdul[1].data / hdul[0].header['EXPTIME']
+        fitsfile = root.replace('exptime', 'rate')+'.fits'
+        hdul.writeto(fitsfile, overwrite=True)
+        rmsmapfile = fitsfile.replace('rate', 'rmsmap')
+        sex_command = 'sex ' + fitsfile +' -c params.sex -DETECT_THRESH 7 -CHECKIMAGE_TYPE BACKGROUND_RMS -CHECKIMAGE_NAME '+rmsmapfile
+        os.system(sex_command) 
+        rmstargetdir = '/'.join(im.split('/')[:-1])+'/'+rmsmapfile
+        print(rmstargetdir)
+        copyfile(rmsmapfile, rmstargetdir)
+        os.system('rm -rf *.cat *.fits')
+os.chdir('../')
+
+
+
+
+
+
+
+IRAF_parallel=True
 if DO_APPHOT:
     """Correct the FITS files by masking cosmic rays, multiply them by their exposure time 
        and write the CR masks to FITS files"""
@@ -323,25 +418,22 @@ if DO_APPHOT:
             hst_filter   = splitted_dir[2]
             if (hst_filter == 'F110W') or (hst_filter == 'F160W'):
                 continue
-            hdu        = fits.open(im)[0]
-            im_exptime = im[:-5]+'_exptime.fits'
-            im_crmask  = im[:-5]+'_crmask.fits'
-            print('Working on file {} from {}'.format(f_count, len(drizzled_astrom_regrid_flist)), end='\r')     
-            #if not 0>np.inf:#(os.path.exists(im_exptime) and os.path.exists(im_crmask)):
-            #   # CR clean
-            #    flist                    = glob(folder+'*drz_sci_regrid.fits')
-            #    hdu.data, sigma, CRmask  = GetCRMasked_exptime(flist, hdu.data, folder, hdu.header['EXPTIME'])
-            #    hdu.writeto(im_exptime, overwrite=True)
-                # Save CRmask to FITS file
-            #    CRmask = CRmask.astype(type(hdu.data[555,555]))
-            #    CR_hdu = hdu.copy()
-            #    CR_hdu.data = CRmask
-            #    CR_hdu.writeto(im_crmask, overwrite=True)
-            #else:
-                # If CRfile and CRcleaned file already exist, read stddevs from file
-            #    stddevs_df = pd.read_csv('../stddevs.txt', delimiter='\t')
-            #    stddevs_df.columns = ['folder', 'stddev']
-            #    stddev = float(stddevs_df.groupby('folder').median().loc[folder])
+            hdu        = fits.open(im)
+            im_crmasked = im[:-5]+'_crmasked.fits'
+            im_crmask  = ''.join(im[:-5].split('_pamcorr')[:-1])+'_crmask.fits'
+#            print('Working on file {} from {}'.format(f_count, len(drizzled_astrom_regrid_flist)), end='\r')
+
+            # Do CR cleaning
+            if   'wfc1' in im:
+                this_wfc_ims = glob(folder+'*wfc1_pamcorr_exptime.fits') 
+            elif 'wfc2' in im:    
+                this_wfc_ims = glob(folder+'*wfc2_pamcorr_exptime.fits') 
+            hdu[1].data, sigma, CRmask = GetCRMasked_exptime(this_wfc_ims, im, folder, hdu[0].header['EXPTIME'])
+            hdu.writeto(im_crmasked, overwrite=True)
+            CR_hdu = hdu.copy()
+            CR_hdu.data = CRmask.astype(float)
+            CR_hdu.writeto(im_crmask, overwrite=True)
+
             # Define magnitude zeropoints...
             zmag  = {'F336W':23.46,'F438W':24.98,'F555W': 25.81, 'F814W': 24.67, 'F656N': 19.92}[hst_filter]-0.1
 #            if DO_APPHOT4:
@@ -357,7 +449,7 @@ if DO_APPHOT:
             centroid_alg = 'centroid'
             coordfile = im.split('_pamcorr')[0]+'.coordfile'
             # Write task per image to do aperture photometry in IRAF
-            iraf_script_images.write('digiphot.apphot.phot image='+im+'[1] ')
+            iraf_script_images.write('digiphot.apphot.phot image='+im_crmasked+'[1] ')
             iraf_script_images.write('coords='+coordfile+' output='+target_dir + ' ')
             iraf_script_images.write('salgori=mode annulus=6 dannulus=3 apertur=5 zmag='+str(zmag) + ' interac=no verify=no ')
             iraf_script_images.write('calgori='+centroid_alg +' cbox=3 datamin=0 datamax=INDEF ')
@@ -369,19 +461,73 @@ if DO_APPHOT:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+DO_IRAF_DF=True
+DO_IRAF_NCR_DF=False
+if DO_IRAF_DF or DO_IRAF_NCR_DF: 
+    """Concatenate all IRAF photometry catalogues into one CSV"""
+    """First do this for the photometry catalogues"""
+    # Get the filelists for all catalogs
+    #if not pms_stars:
+    #    photometry_files  = glob('./IRAF_cats/*/*/*regrid.phot')
+    #    nbadpix_files = glob('./IRAF_cats/*/*/*regrid_nbadpix.phot')
+    #    output_fnames = ['APP_phot_all_exps.csv', 'NBadpix_all_exps.csv']
+    #elif pms_stars:
+    #    photometry_files  = glob('./IRAF_cats/*/*/*regrid_pmsstars.phot')
+    #    nbadpix_files = glob('./IRAF_cats/*/*/*pmsstars_nbadpix.phot')
+    #    output_fnames = ['APP_phot_all_exps_pmsstars.csv', 'NBadpix_all_exps_pmsstars.csv']
+    os.chdir('./working_dir')
+    photometry_files = glob('../IRAF_cats_FLT/*/*/*.phot')
+    #if DO_IRAF_DF and DO_IRAF_NCR_DF:
+    #    todo = [photometry_files,nbadpix_files]
+    #elif DO_IRAF_DF and not DO_IRAF_NCR_DF:
+    #    todo = [photometry_files]
+    #elif DO_IRAF_NCR_DF and not DO_IRAF_DF:
+    #    todo = [nbadpix_files]
+    todo = [photometry_files]    
+    for apphot_files in todo:
+        if apphot_files == photometry_files:
+            which = 0
+        elif apphot_files == nbadpix_files:
+            which = 1
+        # See what columns we need to store
+        columns = ascii.read(apphot_files[0]).to_pandas().columns
+        # Create DF with 5 indexes
+        IRAF_df = pd.DataFrame({'ID':[], 'Filter':[], 'T_Start':[],'Exp_Length':[], 'DrizzleType':[]})
+        IRAF_df = IRAF_df.set_index(['ID', 'Filter', 'T_Start', 'Exp_Length', 'DrizzleType'])
+        # Add the columns that we fill later
+        for col in [w for w in columns if w is not 'ID']:
+            IRAF_df[col] = []
+        # Loop over the photometry files
+        for f_count, apphot_file in enumerate(apphot_files):
+            splitted_dir = apphot_file.split('/')
+            # Extract relevant info for the indexing
+            # Due to structure of directories, most info is in the filepath
+            drizzle_type = 'SingleDrizzle'#('SingleDrizzle' if splitted_dir[1] == 'IRAF_cats' else 'MultiDrizzle')
+            exp_length   = splitted_dir[3]
+            hst_filter   = splitted_dir[2]
+            if drizzle_type=='SingleDrizzle':
+                fname        = splitted_dir[4].split('.')[0]
+                # Lookup associated FITS file
+                hdul         = fits.open(glob('../FLT_exposures/*/*/*'+fname+'.fits')[0])
+                # Extract info from its header
+                t_start      = hdul[0].header['EXPSTART']
+                #obs_date     = hdul[0].header['DATE-OBS']
+            else:
+                t_start      = 'None'
+                obs_date     = 'None'
+    #        print("Filter "  , hst_filter)
+    #        print("Exposure ", fname)
+            print('Working on file {} of {}'.format(f_count, len(apphot_files)), end='\r')
+            # Read in the photometry file
+            phot_df = ascii.read(apphot_file).to_pandas()
+            # Add info to df
+            for col, val in zip(['Filter', 'T_Start', 'Exp_Length', 'DrizzleType'], [hst_filter, t_start, exp_length, drizzle_type]):
+                phot_df[col] = val
+            phot_df = phot_df.set_index(['ID', 'Filter', 'T_Start', 'Exp_Length', 'DrizzleType'])
+            IRAF_df = pd.concat((IRAF_df, phot_df), axis=0)
+            IRAF_df = IRAF_df[IRAF_df.CERROR!='OffImage']
+        IRAF_df.sort_index().to_csv(output_fnames[which])
+IRAF_df.to_csv('subset_flt_photometry.csv') 
 
 
 
