cd62b7f7195a03298225709cf2ee8241b9cd2d89
Luuk Oudshoorn <luuk@lupanas.nl>
2019-11-21 09:10:57 +0100

    New analysis and new sex.py file

--- SExtractor/Analysis/sex.py
+++ SExtractor/Analysis/sex.py
@@ -10,12 +10,7 @@ from astropy.io import ascii
 
 
 
-
-
-# Get the coordinate files
-#from drizzlepac import skytopix
-#from drizzlepac import pixtosky
-
+"""
 
 def get_xy_coords(filepath):
     outputfile = filepath.replace('_pamcorr_exptime.fits', '_all.coordfile')
@@ -29,7 +24,7 @@ def get_xy_coords(filepath):
 photometry_frames = np.sort(glob('../SingleFrame_DetRegrid/WFC*/ib*exptime.fits'))
 Parallel(n_jobs=6)(delayed(get_xy_coords)(i) for i in photometry_frames)
 
-
+"""
 
 
 
@@ -39,7 +34,7 @@ photometry_frames = np.sort(glob('../SingleFrame_DetRegrid/WFC*/ib*exptime.fits'
 force_rerun = True
 
 def run_sex(im):
-    detection_file = im.replace('exptime', 'median')
+    detection_file = im.replace('pamcorr_exptime', 'whitelight_regrid')
     catname = im.split('_pamcorr')[0] + '_phot.fits'
     assoc_file = im.replace('_pamcorr_exptime.fits', '_all.coordfile')
     assoc_df = ascii.read(assoc_file).to_pandas()
@@ -54,7 +49,7 @@ def run_sex(im):
         hdul[1].data = hdul[1].data / exptime
         hdul.writeto(im.replace('exptime', 'rate'), overwrite=True)
     filter_ = hdul[0].header['Filter']
-#    if filter_ !='F656N':
+#    if filter_ !='F555W':
 #        return
     weight_map = im.replace('exptime', 'weight')
     rms_map = im.replace('exptime', 'rms')
@@ -78,7 +73,7 @@ def run_sex(im):
     #sex_command += ' -CHECKIMAGE_NAME '+rms_map
     os.system(sex_command)
 
-a=Parallel(n_jobs=4)(delayed(run_sex)(i) for i in photometry_frames)
+a=Parallel(n_jobs=6)(delayed(run_sex)(i) for i in photometry_frames)
 
 
 
@@ -86,7 +81,7 @@ a=Parallel(n_jobs=4)(delayed(run_sex)(i) for i in photometry_frames)
 
 
 
-"""
+
 
 #### REGRID #####
 
@@ -100,54 +95,96 @@ from astropy.io import fits
 from glob import glob
 import os
 import time
-flist = glob('../F*/*/*wfc1*pamcorr_exptime.fits')
+
 def return_reproject(params):
     image, refim = params
     wfchdu = fits.open(image)[1]
-    fobj   =  fits.open(image.split('_wfc')[0]+'.fits')
+    fobj_root = (image.split('_wfc')[0]).split('/')[-1]
+    fobj_path = glob('../FLT_exposures/*/*/*'+fobj_root+'.fits')[0]
+    fobj   =  fits.open(fobj_path)
     wcs    =  WCS(wfchdu,fobj=fobj)
     
-    ref_hdu = fits.open(refim)[0]
-    ref_wcs = WCS(ref_hdu)
+    ref_hdu = fits.open(refim)[1]
+    ref_wcs = WCS(ref_hdu, fobj=fobj)
 
-    array, interp = reproject_exact((wfchdu.data, wcs), ref_wcs, shape_out=ref_hdu.data.shape, parallel=False)
+    array, interp = reproject_interp((ref_hdu.data, ref_wcs), wcs, shape_out=wfchdu.data.shape)
+    print('Done')
     return array
 
-refim='../../HST_Guido/30dorf814drz.fits'
+flist = glob('./SingleFrame_DetRegrid/WFC*/*pamcorr_rate.fits')
+#### Regrid images to this frame ####
+filters_to_regrid = ['F336W', 'F438W', 'F555W', 'F656N']
+for f in flist[:1]:
+    wfc = ('1' if 'wfc1' in f else '2')
+    rootname = f.split('WFC'+wfc+'/')[1].split('_pamcorr')[0]
+    temp_folder = ''.join(f.split('.fits')[:-1])
+    outputfile = temp_folder+'/'+rootname+'_whitelight_regrid.fits'
+    if os.path.exists(outputfile):
+        continue
+    # Get random number of frames to regrid to this frame
+    reference_images = glob('./SingleFrame_DetRegrid/WFC'+wfc+'/*pamcorr_rate.fits')
+    filters = [fits.open(w)[0].header['FILTER'] for w in reference_images]
+    for filter_ in filters_to_regrid:
+        nframes = 8
+        to_use = np.where([w==filter_ for w in filters])[0]
+        reference_images_use = np.random.choice([reference_images[w] for w in to_use],nframes)
+        
+        final_image = np.zeros((*fits.open(f)[1].data.shape,nframes))
+        #for i,im in enumerate(reference_images_use[:1]):
+        #
+        #    regrid_arr = return_reproject((im,f))
+        #    final_image[:,:,i] = regrid_arr
+        inputs = [(f,w) for w in reference_images_use]
+        returns = Parallel(n_jobs=nframes)(delayed(return_reproject)(i) for i in inputs)
+        for i, data in enumerate(returns):
+            final_image[:,:,i] = data
+        medim_regrid = fits.open(f).copy()
+        medim_regrid[1].data = np.nanmedian(final_image, axis=2)
+        medim_regrid.writeto(temp_folder+'/'+filter_+'median.fits', overwrite=True)
+
+    # Now add the four median images
+    medims = glob(temp_folder+'/*.fits')
+    whitelight_array = np.zeros(fits.open(medims[1])[1].data.shape)
+    for im in medims:
+        whitelight_array += fits.open(im)[1].data
+    whitelight_hdu = fits.open(medims[0])
+    whitelight_hdu[1].data = whitelight_array
+    whitelight_hdu.writeto(outputfile, overwrite=True)
+
+        
+
+###########################################
+flist = glob('./SingleFrame_DetRegrid/WFC2/*pamcorr_rate.fits')
+
+
+refim='../FLT_exposures/REGRID/Whitelight_Images/wfc2/whitelight_wfc2.fits'
 t0=time.time()
 
 for iter_,image in enumerate(flist):
+    if fits.open(image)[0].header['EXPTIME'] < 31:
+        continue
+    if os.path.exists(image.replace('_pamcorr_rate', '_whitelight_regrid')):
+        continue
+    inputs = (image, refim)
 
-    im_wfc1 = image
-    im_wfc2 = image.replace('wfc1', 'wfc2')
 
-    imname = './Add_Regrid/' + im_wfc1.split('_flt')[0].split('/')[-1] + '_add_regrid.fits'
+    result = return_reproject(inputs)#Parallel(n_jobs=num_cores)(delayed(return_reproject)(i) for i in inputs)
+    whitelight_regrid = fits.open(image).copy()
+    whitelight_regrid[1].data = result
+    whitelight_regrid.writeto(image.replace('_pamcorr_rate', '_whitelight_regrid'), overwrite=True)
 
-    #if os.path.exists(imname):
-    #    print(imname, 'done')
-    #    continue
-    inputs = [(im_wfc1, refim), (im_wfc2, refim)]
-
-    num_cores = 2
-    results = Parallel(n_jobs=num_cores)(delayed(return_reproject)(i) for i in inputs)
-
-    final_image = np.zeros((*results[0].shape,2))
-    final_image[:,:,0] = results[0]
-    final_image[:,:,1] = results[1]
-    final_image = np.nansum(final_image, axis=2) / fits.open(image)[0].header['EXPTIME']
-
-    ref_hdu = fits.open(refim)[0]
-    ref_wcs = WCS(ref_hdu.header)
+    #ref_hdu = fits.open(refim)[0]
+    #ref_wcs = WCS(ref_hdu.header)
 
     
-    primary_hdu = fits.PrimaryHDU(data=None, header=fits.open(im_wfc1)[0].header)
-    output_hdul = fits.HDUList([])
+    #primary_hdu = fits.PrimaryHDU(data=None, header=fits.open(im_wfc1)[0].header)
+    #output_hdul = fits.HDUList([])
     
-    output_hdul.append(ref_hdu.copy())
-    output_hdul[0].data=final_image
-    output_hdul[0].header['FILTER']='WARNING_USE_OTHER_EXTENSION'
-    output_hdul.append(primary_hdu)
-    output_hdul.writeto(imname, overwrite=True)
+    #output_hdul.append(ref_hdu.copy())
+    #output_hdul[0].data=final_image
+    #output_hdul[0].header['FILTER']='WARNING_USE_OTHER_EXTENSION'
+    #output_hdul.append(primary_hdu)
+    #output_hdul.writeto(imname, overwrite=True)
     print('ETA: ', (len(flist)-iter_-1) * ((time.time()-t0)/(iter_+1))/60)
 
 """
